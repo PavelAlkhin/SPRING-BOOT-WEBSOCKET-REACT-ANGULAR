1dcfdb7d78497b239efa99a682baabda3d68032e
Pavel <unicon2000@gmail.com>
2023-09-22 21:06:25 +0500

    init

---
+++ locatestats-back/src/main/java/com/cy/tiersec/websocket/locatestats/LocatestatsApplication.java
@@ -0,0 +1,13 @@
+package com.cy.tiersec.websocket.locatestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocatestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocatestatsApplication.class, args);
+	}
+
+}
---
+++ locatestats-back/src/test/java/com/cy/tiersec/websocket/locatestats/LocatestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.tiersec.websocket.locatestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocatestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
